1e5c20771ae1078eca583caecd7a1e1e8cfe12ec
Seifeldin2510 <seifeldin.ashraf@yahoo.com>
2023-06-07 20:44:05 +0300

    Test for Notification and Transaction balance

--- src/main/test/com/payup/test/TransactionTest.java
+++ src/main/test/com/payup/test/TransactionTest.java
@@ -23,7 +23,6 @@ public class TransactionTest {
     Client i = new Client("Saeed","Elhawa","HappyTheAir","9876","1200",48512,"8890","Business");
     Client j = new Client("Snoop","Nogg","SnoopNogg69","9876","4001",300000,"1092","Customer");
 
-
     @Before
     public void setUp() throws Exception {
     }
@@ -32,99 +31,42 @@ public class TransactionTest {
     public void tearDown() throws Exception {
     }
 
-//    @Test
-//    public void testDeposit() {
-//        String s = T.deposit(c,"410",1000);
-//        assertFalse("Amount should be less than 0",!(s.equals("Amount Deposited Successfully!")));
-//
-//    }
-//
-//
-//    @Test
-//    public void test2Deposit() {
-//        String s = T.deposit(d,"7500",-1000);
-//        assertFalse("Amount should be less than 0",!(s.equals("Amount Entered must be more than 0")));
-//    }
-//
-//    @Test
-//    public void testWithdraw() {
-//       String s =  T.withdraw(a,"120",20000);
-//       assertTrue("Amount should be less than balance",s.equals("Amount Withdrawn Successfully!"));
-//    }
-//
-//    @Test
-//    public void test2Withdraw() {
-//        String s =  T.withdraw(b,"300",100000000);
-//        assertTrue("Amount should be less than balance",s.equals("Withdraw failed"));
-//    }
-
     @Test
     public void transfer() {
         String s = T.transfer(e,f,"1177","1240",1000,"7700");
-        assertTrue("Amount should be less than balance of first account",s.equals("Transaction Completed Successfully!"));
+        assertEquals(38000,e.getAccounts().get(e.findAccount("1177")).getBalance(),0.0f);
 
     }
 
     @Test
     public void test2Transfer() {
         String s = T.transfer(g,h,"1230","9000",100000000,"6004");
-        assertTrue("Amount should be less than balance of first account",s.equals("Transaction Failed!"));
+        assertEquals(42300,g.getAccounts().get(g.findAccount("1230")).getBalance(),0.0f);
+
 
     }
-    @Test
-    public void test3Transfer() {
-        String s = T.transfer(a,b,"120","3000",10000,"9876");
-        assertTrue("Check Account number of receiver",s.equals("Account number of receiver is not found"));
-
-    }
-    @Test
-    public void test4Transfer() {
-        String s = T.transfer(c,d,"4100","7500",10000000,"9871");
-        assertTrue("Check Account number of sender",s.equals("Account number of sender is not found"));
-
-    }
-
-
-//    @Test
-//    public void testLoan() {
-//        String s =T.loan(i,"1200",1000,"8890");
-//        assertFalse("Amount should be less than 100000",!(s.equals("Loan Accepted")));
-//
-//    }
-//    @Test
-//    public void test2Loan() {
-//        int x =j.findAccount("4001");
-//        j.getAccounts().get(x).setLoanAmount(1000000);
-//        String s = T.loan(j,"4001", 400000,"1092");
-//        assertFalse("Amount should be less than 100000", !(s.equals("Loan Rejected")));
-//    }
-
-
 
     @Test
     public void testOnlinePurchase() {
         String s =T.onlinePurchase(j,"4001","1092",1000);
-        assertFalse("Not enough Balance", !(s.equals("Transaction Successful!")));
+        assertEquals(299000,j.getAccounts().get(j.findAccount("4001")).getBalance(),0.0f);
     }
 
     @Test
     public void test2OnlinePurchase() {
         String s =T.onlinePurchase(b,"300","1988",100000);
-        assertFalse("Not enough Balance", !(s.equals("Transaction Failed!")));
+        assertEquals(99999,b.getAccounts().get(b.findAccount("300")).getBalance(),0.0f);
     }
-
 
     @Test
     public void testPayBill(){
         String s = T.payBill(c,"410", 20000,"9871");
-        assertTrue("Insufficient Funds",s.equals("Bill Paid Successfully"));
-
+        assertEquals(30505,c.getAccounts().get(c.findAccount("410")).getBalance(),0.0f);
     }
+
     @Test
     public void test2Paybill(){
-
         String s = T.payBill(a,"120", 20000000,"9876");
-        assertFalse("Insufficient Funds",!(s.equals("Insufficient Funds")));
-
+        assertEquals(30000,a.getAccounts().get(a.findAccount("120")).getBalance(),0.0f);
     }
 }
